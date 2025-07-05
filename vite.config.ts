f7212206e6dccfce412a0c927544b106f21b65a7
plntry <nedilko7b@gmail.com>
2025-07-05 17:18:42 +0300

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "serve" ? "/" : "/map-places-app/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
@@ -15,4 +16,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-});
+}));
